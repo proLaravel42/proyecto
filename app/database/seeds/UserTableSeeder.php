ce4c74dc24136878174e640f2e20e35b0c155179
Fernando <seifer_013@hotmail.com>
2015-06-17 21:07:06 -0500

    Agregamos pantallas y colocamos la clase active

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -6,14 +6,13 @@ class UserTableSeeder extends Seeder {
 	public function run()
 	{
 		User::create([
-			'first_name' => 'Alicia',
-			'last_name'  => 'Guzman',
-			'username'   => 'alicia.guzman',
-			'email'      => 'ezeezegg@gmail.com',
-			'password'   => 'admin'
+			'first_name' => 'Fernando',
+			'last_name'  => 'Galarza Rodriguez',
+			'username'   => 'seifer13',
+			'email'      => 'seifer_013@hotmail.com',
+			'password'   => '12345'
 			//'password'   =>  Hash::make('admin')
 			]);
 		}
-		
-		
+
 	}
--- app/routes.php
+++ app/routes.php
@@ -1,13 +1,20 @@
 <?php
 
 
+/*define response for route not exist*/
+App::missing(function($exception)
+{
+    return Response::view('missing', array(), 404);
+});
+/*define response for route not exist*/
+
 
 /*Controls of auth*/
 Route::get('login', 'AuthController@showLogin');
 Route::post('login', 'AuthController@postLogin');
 Route::get('logout', 'AuthController@logout');
 
- //show view new user
+Route::get('showRegister', 'AuthController@registerUser'); //show view new user
 Route::post('sign-up', ['as' => 'register', 'uses' => 'UserController@register' ] );//register new user
 
 Route::get('passRecovery', 'UserController@showPassRecovery');
@@ -22,30 +29,13 @@ Route::post('password/reset/{token}', ['as' => 'resetPass', 'uses' => 'UserContr
 /*private routes only for users auth*/
 Route::group(['before' => 'auth'], function()
 {
-	Route::get('/', 'HomeController@showWelcome');
-	Route::get('showRegister', 'AuthController@registerUser');
+	//Route::get('/', 'HomeController@showWelcome');
 
 	Route::get('dash', 'AuthController@showWelcome');
 
 	Route::post('updateUser', ['as' => 'updateUser', 'uses' => 'UserController@updateUser' ] );
 
 	Route::post('uploadImage', ['as' => 'uploadImage', 'uses' => 'UserController@uploadImage' ] );
-
-	Route::post('asignarTarea', ['as' => 'asignarTarea', 'uses' => 'TareasController@saveTarea' ] );
-
-	Route::get('getUsers', 'UserController@getUsers');
-	Route::get('getTasks', 'TareasController@getTasks');
-	Route::get('getTasksSuperAdmin', 'TareasController@getTasksSuperAdmin');
-	Route::get('listUsers', 'UserController@listUsers');
-	Route::get('deleteUser/{id}', 'UserController@deleteUser');
-	Route::post('uploadpdf', ['as' => 'uploadpdf', 'uses' => 'TareasController@uploadpdf' ] );
-	Route::get('cleanDD', ['as' => 'cleanDD', 'uses' => 'TareasController@cleanDD' ] );
-	Route::get('deleteTask/{id}', ['as' => 'deleteTask', 'uses' => 'TareasController@deleteTask' ] );
-	Route::get('getTaskDetailsById/{id}', ['as' => 'getTaskDetailsById', 'uses' => 'TareasController@getTaskDetailsById' ] );
-	Route::post('updateTask/{id}', ['as' => 'updateTask', 'uses' => 'TareasController@updateTask' ] );
-	Route::post('sendRejectTask', ['as' => 'sendRejectTask', 'uses' => 'TareasController@sendRejectTask' ] );
-	Route::get('search', ['as' => 'search', 'uses' => 'TareasController@search' ] );
-	
 
 	//Route::post('uploadImage', 'UserController@uploadImage');
 });
